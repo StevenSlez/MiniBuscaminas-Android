b73abb2b934dfe446ace21af2135ebdd1fe58f13
StevenSlez <slezak87@gmail.com>
2023-11-11 19:45:58 +0100

    Commit 11/11/2023

--- app/src/main/java/com/example/minibuscaminas_android/MainActivity.java
+++ app/src/main/java/com/example/minibuscaminas_android/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.minibuscaminas_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn00, btn01, btn02, btn03, btn10, btn11, btn12, btn13, btn20, btn21, btn22, btn23, btn30, btn31, btn32, btn33, btnResolver, btnNuevo;
+    TextView lblPuntos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
